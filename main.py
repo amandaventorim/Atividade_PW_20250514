4bb53b749e401e253e9c9bf68cffa9d6cf77fc49
amanda <ventorim.amanda@gmail.com>
2025-05-14 18:15:58 -0300

    tarefa finalizada

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import uvicorn
 
 from data import produto_repo
 from data import cliente_repo
+from data import categoria_repo
 
 
 app = FastAPI()
@@ -13,6 +14,7 @@ templates = Jinja2Templates(directory="templates")
 
 produto_repo.criar_tabela()
 cliente_repo.criar_tabela()
+categoria_repo.criar_tabela()
 
 
 @app.get("/")
@@ -21,13 +23,23 @@ async def get_root():
     response = templates.TemplateResponse("index.html", {"request": {}, "produtos": produtos})
     return response
 
-
 @app.get("/clientes")
 async def get_clientes():
     clientes = cliente_repo.obter_todos()
     response = templates.TemplateResponse("clientes.html", {"request": {}, "clientes": clientes})
     return response
 
+@app.get("/produtos")
+async def get_produtos():
+    produtos = produto_repo.obter_todos()
+    response = templates.TemplateResponse("produtos.html", {"request": {}, "produtos": produtos})
+    return response
+
+@app.get("/categorias")
+async def get_categorias():
+    categorias = categoria_repo.obter_todos()
+    response = templates.TemplateResponse("categorias.html", {"request": {}, "categorias": categorias})
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
